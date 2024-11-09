c577c89e58551f21a1fe1600a0ceee584be2b322
princeey1 <prince-david.madzou@polymtl.ca>
2024-11-09 10:31:53 -0500

    added a soltion class

--- Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
+++ Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
@@ -3,12 +3,53 @@ import random
 import time
 import math
 
+
+
+
+class Solution:
+
+    def __init__(self,schedule: Schedule):
+        self.schedule = schedule
+        self.best_solution = None
+        self.N = len(self.schedule.conflict_list)
+        self.E = len(self.schedule.conflict_list)
+        
+    def __call__(self, *args, **kwds):
+        ...
+
+    def _parse_solution_to_schedule(self):
+        ...
+
+    def _generate_solution(self):
+        ...
+
+    def _gen_valid(self):
+        ...
+
+    def _check_redondant_vistor(self):
+        ...
+
+    def _select(self):
+        ...
+
+    def _evaluation(self):
+        ...
+    
+    def solve(self):
+        ...
+
+    
+
+        pass
+
 def solve(schedule : Schedule) -> dict:
     """
     Your solution of the problem
     :param schedule: object describing the input
     :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
     """
+
+    print(schedule.course_list)
     # Add here your agent
     
     # return hill_climbing()
