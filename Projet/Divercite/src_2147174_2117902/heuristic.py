69efdb8b54ed372baa4b49697f241e0ce3225679
princeey1 <prince-david.madzou@polymtl.ca>
2024-11-15 04:52:13 -0500

    added pieces heuristic

--- Projet/Divercite/src_2147174_2117902/heuristic.py
+++ Projet/Divercite/src_2147174_2117902/heuristic.py
@@ -1,6 +1,7 @@
 from .definition import AlgorithmHeuristic, Heuristic,ARGS_KEYS
 from .constant import *
 from .helper import *
+import numpy as np
 
 class PointDifferenceHeuristic(AlgorithmHeuristic):
 
@@ -27,5 +28,53 @@ class ControlIndexHeuristic(AlgorithmHeuristic):
     def _evaluation(self, current_state, **kwargs):
         ...
 
-class RessourcesVarianceHeuristic(AlgorithmHeuristic):
-    ...
+class PiecesVarianceHeuristic(AlgorithmHeuristic):
+    
+    def __init__(self,city_weight=.7,ress_weight=.3):
+        super().__init__(-20, 0,L=5.5)
+        self.city_weight = city_weight
+        self.ress_weight =ress_weight
+
+    def _evaluation(self, current_state, **kwargs):
+        opponent_pieces = current_state.players_pieces_left[kwargs['opponent_id']]
+        my_pieces = current_state.players_pieces_left[kwargs['my_id']]
+
+        my_original_pieces = kwargs['my_pieces']
+        opponent_original_pieces = kwargs['opponent_pieces']
+
+        #####
+
+        my_original_var = self._pieces_var(my_original_pieces)
+        opponent_original_var = self._pieces_var(opponent_original_pieces)
+
+        my_state_var = self._pieces_var(my_pieces)
+        opp_state_var = self._pieces_var(opponent_pieces)
+
+        opp_diff = opp_state_var-opponent_original_var
+        state_diff = opp_state_var-my_state_var
+        my_diff = my_state_var-my_original_var
+
+        return -my_state_var
+        
+    def _pieces_var(self,pieces:dict[str,int]):
+        city_val = np.array([pieces[cn] for cn in CityNames._member_names_])
+        ress_val = np.array([pieces[rn] for rn in RessourcesNames._member_names_])
+        
+        
+        ##################################
+        # city_val = np.var(city_val)
+        # ress_val = np.var(ress_val)*1.05
+        
+        # ress_val = 0.5 if ress_val == 0 else ress_val
+        #return -min(city_val, ress_val)
+        #return float(city_val*self.city_weight+ ress_val*self.ress_weight)/self._h_tot_weight
+        #return float(city_val+ress_val)
+        ##########################
+        pieces_vectors = (city_val*self.city_weight + ress_val*self.ress_weight)/self._h_tot_weight
+        return float(np.var(pieces_vectors)*100)
+
+
+    
+    @property
+    def _h_tot_weight(self):
+        return self.city_weight+self.ress_weight
